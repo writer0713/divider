2082ac4ec5b8718d4238e4f0b0d330bf63bae3f1
KIM JUNGHWAN <writer0713@naver.com>
2019-07-07 06:39:25 +0900

    조 개수로 나누기 완성

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,6 @@ export class AppComponent {
   title = "GROUP DIVIDER";
 
   maxNumber: number;
-  numberPerGroup: number;
   numberOfGroup: number;
   disableButton: boolean = true;
 
@@ -24,10 +23,18 @@ export class AppComponent {
     }
   }
 
+  /**
+   * 모든 input에 입력했는지 확인
+   */
   hasAllInputs(): boolean {
     return this.maxNumber > 0 && this.numberOfGroup > 0;
   }
 
+  /**
+   * 입력한 숫자가 올바른지 확인
+   *
+   * 최대인원수 >= 조 개수
+   */
   hasProperNumbers(): boolean {
     return this.maxNumber >= this.numberOfGroup;
   }
@@ -37,29 +44,42 @@ export class AppComponent {
     this.generateNumbers();
 
     let total: number = this.maxNumber;
-    let numberOfGroup: number = this.numberOfGroup;
+    let numberOfGroup: number = this.numberOfGroup; // 조 개수
     let numberPerGroup: number = total / numberOfGroup; // 한 조당 조원수
     let rest: number = total % numberOfGroup; // 남은 아이들
 
     for (let i = 1; i <= numberOfGroup; i++) {
-      const group: number[] = [];
-      for (let k = 1; k <= numberPerGroup; k++) {
-        group.push(this.tempNumbers.pop());
-      }
-      group.sort((a, b) => a - b);
-
+      const group: number[] = this.divideFirst(numberPerGroup);
       this.results.push(group);
     }
 
-    for (let z = 1; z <= rest; z++) {
-      const group: number[] = this.results[z];
-      group.push(this.tempNumbers.pop());
-      group.sort((a, b) => a - b);
-    }
-
-    console.log(this.results);
+    this.divideRest(rest);
   }
 
+  divideFirst(numberPerGroup: number): number[] {
+    const group: number[] = [];
+    for (let k = 1; k <= numberPerGroup; k++) {
+      group.push(this.tempNumbers.pop());
+    }
+    group.sort(this.ascending);
+    return group;
+  }
+
+  divideRest(rest: number) {
+    for (let i = 1; i <= rest; i++) {
+      const group: number[] = this.results[i];
+      group.push(this.tempNumbers.pop());
+      group.sort(this.ascending);
+    }
+  }
+
+  ascending(a: number, b: number) {
+    return a - b;
+  }
+
+  /**
+   * 최대 번호만큼의 인원수(번호)를 생성
+   */
   generateNumbers() {
     const maxNumber: number = this.maxNumber;
     for (let i = 1; i <= maxNumber; i++) {
@@ -68,16 +88,18 @@ export class AppComponent {
     this.shuffle(this.tempNumbers);
   }
 
+  /**
+   * 리셋
+   */
   reset() {
     this.tempNumbers = [];
     this.results = [];
   }
 
-  getRandomInt(max: number) {
-    const min: number = 1;
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
+  /**
+   *
+   * @param numbers number array 셔플
+   */
   shuffle(numbers: number[]) {
     for (let i = numbers.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
