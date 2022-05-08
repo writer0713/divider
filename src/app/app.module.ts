79a4798d40a22a5ec93f292a543aff26140979ed
tyler.kim <writer0713@naver.com>
2022-05-08 14:49:26 +0900

    feat: angular9 으로 업그레이드 (#3)
    - angular9 으로 업그레이드

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
-import {
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatExpansionModule,
-  MatInputModule,
-  MatIconModule,
-  MatDialogModule,
-  MatTooltipModule,
-} from "@angular/material";
+import { MatButtonModule } from "@angular/material/button";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatChipsModule } from "@angular/material/chips";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
